c029ff57a14f2cb269676969bd0e979ab075bd1d
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2018-12-14 23:31:31 -0200

    Implementação de Pedido, Pagamento, PgtoBoleto e PgtoCredito

--- src/main/java/com/cursojava/cursomc/domain/Cliente.java
+++ src/main/java/com/cursojava/cursomc/domain/Cliente.java
@@ -18,23 +18,26 @@ import com.cursojava.cursomc.domain.enums.TipoCliente;
 @Entity
 public class Cliente {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
-	
-	@OneToMany(mappedBy="cliente")
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
-	//Um set (conjunto) não aceita repetição.
+
+	// Um set (conjunto) não aceita repetição.
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
+
+	@OneToMany(mappedBy="cliente")
+	public List<Pedido> pedidos = new ArrayList<>();
+
 	public Cliente() {
-		
+
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
@@ -101,10 +104,38 @@ public class Cliente {
 	public Set<String> getTelefones() {
 		return telefones;
 	}
-	
-	
-	
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
