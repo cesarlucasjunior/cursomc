700edf07c5ae2444e55533064aee00ce50674ca6
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-01-25 11:51:42 -0200

    Implementando ClienteService e ClienteResource

--- src/main/java/com/cursojava/cursomc/dto/PedidoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.cursojava.cursomc.dto;
-
-public class PedidoDTO {
-	
-	private Integer id;
-	private boolean pagamento;
-	private Double valor;
-
-}
--- src/main/java/com/cursojava/cursomc/services/ClienteService.java
+++ src/main/java/com/cursojava/cursomc/services/ClienteService.java
@@ -1,34 +1,66 @@
 package com.cursojava.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursojava.cursomc.domain.Categoria;
 import com.cursojava.cursomc.domain.Cliente;
+import com.cursojava.cursomc.dto.ClienteDTO;
 import com.cursojava.cursomc.repositories.ClienteRepository;
+import com.cursojava.cursomc.services.exception.DataIntegrityException;
 import com.cursojava.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
-	//A classe tipo Service é uma ponte entre os dados e os endpoint. Nela, separamos a responsabilidade
-	//de buscar um dados específico e disponibiliza-la ao resource.
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
-	// Aqui teremos o método que acessará os dados enviando o resultado ao REST:
-	
-		public Cliente find(Integer id) {
-			
-			Optional<Cliente> obj = clienteRepository.findById(id);
-			
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id " + id +
-					", Tipo: " + Categoria.class.getName()));
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return clienteRepository.save(cliente);
+	}
+
+	public Cliente update(Cliente cliente) {
+		find(cliente.getId());
+		return clienteRepository.save(cliente);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possível excluir esse cliente porque ela possui outra tabela associada!");
 		}
-		
-	
-	
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
 
 }
