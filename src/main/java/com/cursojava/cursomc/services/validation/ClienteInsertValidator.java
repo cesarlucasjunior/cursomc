fff68f59748f2c8fcafd627f1a7839c81ab38618
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-01-29 11:30:30 -0200

    Implementação validação da unicidade do e-mail

--- src/main/java/com/cursojava/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursojava/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursojava.cursomc.domain.Cliente;
 import com.cursojava.cursomc.domain.enums.TipoCliente;
 import com.cursojava.cursomc.dto.ClienteInsertDTO;
+import com.cursojava.cursomc.repositories.ClienteRepository;
 import com.cursojava.cursomc.resource.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteInsertDTO> {
 
 	//Essa classe é responsável por especificar a validação em si, ou seja, é o que tem "dentro" da anotação.
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	public void initialize(ClienteInsert cliente) {
 	}
@@ -30,13 +37,21 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				&& !CpfCnpjValidation.isValidCNPJ(clienteDTO.getCpfOuCnpj())) {
 			lista.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
 		}
+		
+		//Validação de e-mail:
+		
+		Cliente cliente = clienteRepository.findByEmail(clienteDTO.getEmail());
+		
+		if(cliente != null) {
+			lista.add(new FieldMessage("email", "Esse e-mail já está cadastrado!"));
+		}
 
 		for (FieldMessage e : lista) {
 			contexto.disableDefaultConstraintViolation();
 			contexto.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
-
+		
 		return lista.isEmpty();
 	}
 
