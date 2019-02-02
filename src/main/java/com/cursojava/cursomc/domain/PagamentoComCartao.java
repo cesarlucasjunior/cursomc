d2f6c7f748655fdf512e5ee4324ccfe57fe580cb
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-02-01 23:55:03 -0200

    Alteracao Tipo Pagamento Cartao Jackson Config

--- src/main/java/com/cursojava/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/cursojava/cursomc/domain/PagamentoComCartao.java
@@ -6,7 +6,7 @@ import com.cursojava.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
-@JsonTypeName("pagamentoComBoleto")
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
